9b814fb9657db8f318dceaf05c41ec6ffec5ead0
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-04-28 19:49:18 -0300

    fix - corrigindo o bug do menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.png";
 import { useNavigate } from "react-router-dom";
 import SocialMedia from "./SocialMedia";
-// import { Squash as Hamburger } from "hamburger-react";
+import { Squash as Hamburger } from "hamburger-react";
 
 function Header() {
   const navigate = useNavigate();
+  const [statusToggle, setToggle] = useState(false);
 
-  const hamburguer = document.querySelector('#hamburguer');
-  const menu = document.querySelector('#menu');
+  const menu = document.querySelector("#menu");
 
   const handleClick = () => {
-    if (!menu.className) {
-      menu.classList.toggle('active');
-    } else {
-      menu.classList.remove('active');
-    }
-
-    if (!hamburguer.className) {
-      hamburguer.classList.toggle('active');
-    } else {
-      hamburguer.classList.remove('active');
-    }
-  }
+    statusToggle ? setToggle(false) : setToggle(true);
+  };
+  
+  console.log(statusToggle);
 
   return (
     <header id="header">
       <img id="logoMain" src={logo} alt="Logo Canoé Digital" />
 
-      <div
-        id="hamburguer"
-        onClick={ handleClick }
-      >
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </div>
+      <Hamburger
+        label="Show menu"
+        color="#f7f7f7"
+        size={20}
+        onClick={handleClick}
+        onToggle={statusToggle => {
+          if (statusToggle) {
+            menu.className = "active";
+          } else {
+            menu.classList.remove("active");
+          }
+        }}
+      />
 
       <nav id="menu">
         <h3 onClick={() => navigate("/")}>Home</h3>
