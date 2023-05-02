f7f0b58c787158d6a0c671176aa8ad15af7c5ffb
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-05-02 18:18:50 -0300

    fix - correção no menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,30 +9,29 @@ function Header() {
   const [statusToggle, setToggle] = useState(false);
 
   const menu = document.querySelector("#menu");
-
-  const handleClick = () => {
-    statusToggle ? setToggle(false) : setToggle(true);
-  };
   
   console.log(statusToggle);
+  console.log(menu);
 
   return (
     <header id="header">
       <img id="logoMain" src={logo} alt="Logo Canoé Digital" />
 
-      <Hamburger
-        label="Show menu"
-        color="#f7f7f7"
-        size={20}
-        onClick={handleClick}
-        onToggle={statusToggle => {
-          if (statusToggle) {
-            menu.className = "active";
-          } else {
-            menu.classList.remove("active");
-          }
-        }}
-      />
+      <div
+        id="hamburguer"
+        onClick={() => statusToggle === true ? setToggle(false) : setToggle(true)}
+      >
+        <Hamburger
+          label="Show menu"
+          color="#f7f7f7"
+          size={20}
+          onToggle={(toggle = statusToggle) => {
+            if (toggle) {
+              menu.classList.toggle('active')
+            }
+          }}
+        />
+      </div>
 
       <nav id="menu">
         <h3 onClick={() => navigate("/")}>Home</h3>
