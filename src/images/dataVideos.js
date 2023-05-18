6cdf8c4d122352acf67813358598f92f39364c0b
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-05-18 14:17:49 -0300

    add - video a voz que clama no sertão

--- src/images/dataVideos.js
+++ src/images/dataVideos.js
@@ -1,5 +1,5 @@
 export const videos = [
-  "https://www.youtube.com/embed/XtrBPn_pkrw",
+  "https://www.youtube.com/embed/LvAzYFbR-kA",
   "https://www.youtube.com/embed/y_sPD-VZ4-s",
   "https://www.youtube.com/embed/Lkao4ZYbbs8",
 ];
