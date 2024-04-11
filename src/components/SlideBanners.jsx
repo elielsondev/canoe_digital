231d555f418366db97936d5ecfb56a0ae531fbae
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-04-11 19:14:59 -0300

    feat - create component SlideBanners

---
+++ src/components/SlideBanners.jsx
@@ -0,0 +1,11 @@
+import bannerHome from "../images/banner_home.png";
+
+function SlideBanners() {
+  return (
+    <figure>
+        <img className="mainBanner" src={bannerHome} alt="banner" />
+    </figure>
+  )
+}
+
+export default SlideBanners;
