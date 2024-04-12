70f1b9a1794f5f42a7b1614fa51560c3b0ed4f73
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-04-12 09:49:38 -0300

    feat - inclusion of advertisers and images

--- src/data/patrocinadores.js
+++ src/data/patrocinadores.js
@@ -1,21 +1,24 @@
-import anunciante1 from '../images/ANUNCIE-AQUI.png';
+import anunciante1 from '../images/sangraf.jpg';
+import anunciante2 from '../images/sangraf2.jpeg';
+import anunciante3 from '../images/ANUNCIE-AQUI.png';
+import anunciante4 from '../images/ANUNCIE-AQUI.png';
 
 const patrocinadores = [
     {
-        nome: "Divulgue Aqui 1",
+        nome: "Sangraf - Fazendo acontecer!",
         banner: anunciante1,
     },
     {
-        nome: "Divulgue Aqui 2",
-        banner: anunciante1,
+        nome: "Sangraf - Fazendo acontecer!",
+        banner: anunciante2,
     },
     {
         nome: "Divulgue Aqui 3",
-        banner: anunciante1,
+        banner: anunciante3,
     },
     {
         nome: "Divulgue Aqui 4",
-        banner: anunciante1,
+        banner: anunciante4,
     }
 ];
 
