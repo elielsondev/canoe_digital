253ccd79ed24a282c28317dc61017490af11b424
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-04-12 09:23:56 -0300

    feat - date inclusion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,12 @@ import SocialMedia from "./SocialMedia";
 import logo from '../images/logo.png'
 
 function Footer() {
+  const data = new Date().getYear() + 1900;
   return (
     <footer id="footer">
       <div>
         <img src={logo} alt="Logo" width="3%" />
-        <strong>CANOÉ DIGITAL FM © 2023 - Todos os direitos reservados.</strong>
+        <strong>CANOÉ DIGITAL FM © {data} - Todos os direitos reservados.</strong>
         <img src={logo} alt="Logo" width="3%" />
       </div>
       <h6>Site criado por Elielson Nascimento</h6>
